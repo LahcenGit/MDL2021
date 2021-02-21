36a765aa862e1299dceb8abe2d8fdb0a189e6469
HindBenos <62807209+HindBenos@users.noreply.github.com>
2021-02-22 00:05:11 +0100

    Update RegisterController.php

--- app/Http/Controllers/Auth/RegisterController.php
+++ app/Http/Controllers/Auth/RegisterController.php
@@ -83,6 +83,8 @@ class RegisterController extends Controller
         
         $user->save();
 
+        
+
         $professionnel = new professionnel();
       
         $professionnel->entreprise = $data['entreprise'];
