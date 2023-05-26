5f1209df05b295e176eb923a6d386aacd05ea725
Benosman Hind <62807209+BenosmanHind@users.noreply.github.com>
2023-05-26 15:16:45 +0200

    Update User.php

--- app/Models/User.php
+++ app/Models/User.php
@@ -51,5 +51,9 @@ class User extends Authenticatable
     {
         return $this->hasOne(Particulier::class);
     }
+    public function deliveryOrder()
+    {
+        return $this->hasMany(Delivryorder::class);
+    }
 
 }
